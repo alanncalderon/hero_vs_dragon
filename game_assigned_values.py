e98c2027e5e09f3346f26dc2cd5eb4146db048a0
alanncalderon <alan2010c@hotmail.com>
2022-03-07 22:49:35 +0100

    two version of the code. One with input values and ther other one with assigned values.

--- game_assigned_values.py
+++ game_assigned_values.py
@@ -14,26 +14,28 @@ except ValueError:
 except:
     print("this is a new error that I did not see")
 
+print("The dragon with", dragon_hp, "hp attacks out hero with", hero_hp, "hp")
 
+# add a While for an infinite block
 while True:
-    print("The dragon with", dragon_hp, "hp attacks out hero with", hero_hp, "hp")
     dragon_attack = random.randint(1, dragon_max_dmg)
-    hero_hp = hero_hp - dragon_attack
-    if hero_hp <= 0:
-        print("The dragon does", dragon_attack, "damage and the hero has", hero_hp, "hp left")
-    # add an if condition to check if the hero was killed by the dragon
+    # here you need to update the hero hp, you need to subtract the damage that the dragon did
+    hero_hp= hero_hp-dragon_attack
 
+    print("The dragon does", dragon_attack, "damage and the hero has", hero_hp, "hp left")
+    # add an if condition to check if the hero was killed by the dragon
+    if hero_hp<=0:
         print("Unfortunately the dragon killed our hero. RIP sir Bravealot")
         break
 
     hero_attack = random.randint(1, hero_max_dmg)
-    dragon_hp = dragon_hp - hero_attack
-    print("The hero does {} damage and the dragon has {} hp left".format(hero_attack, dragon_hp))
-    if dragon_hp <= 0:
     # here you need to update the dragon hp, you need to subtract the damage that the hero did
+    dragon_hp= dragon_hp-hero_attack
 
-
-        print("The hero does", hero_attack, "damage and the dragon has", dragon_hp, "hp left")
+    print("The hero does", hero_attack, "damage and the dragon has", dragon_hp, "hp left")
     # add an if condition to check if the dragon was killed by the hero
-
+    if dragon_hp<=0:
         print("Our valiant hero has slain the dragon!")
+        break
+    continue
+    input("Round over. Press any key")
